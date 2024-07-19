a681e126285f399857d83b038a3eb0ca5a704b30
Manon Morin <manon.morin@ymail.com>
2024-07-19 16:05:18 -0700

    python_scripts_reformated

--- Dataset_generation/scripts/extract_mpileup_info.py
+++ Dataset_generation/scripts/extract_mpileup_info.py
@@ -4,34 +4,36 @@ import pandas as pd
 
 
 def extract_mpileup_info(input_file, output_file):
-        # Dynamically determine the number of initial comment lines to skip
+    # Dynamically determine the number of initial comment lines to skip
     with open(input_file) as file:
-        skip_count = sum(1 for line in file if line.startswith('#'))
+        skip_count = sum(1 for line in file if line.startswith("#"))
 
     # Read the mpileup file into a pandas DataFrame
     # Assuming the mpileup file has no header and columns are separated by tabs
-    col_names = ["CHROM", "POS", "ID", "REF", "ALT","QUAL","FILTER","INFO","FORMAT","SAMPLE"]
-    mpileup_data = pd.read_csv(input_file, sep='\t', header=None,
-                               names=col_names,skiprows=skip_count)
+    col_names = ["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT", "SAMPLE"]
+    mpileup_data = pd.read_csv(
+        input_file, sep="\t", header=None, names=col_names, skiprows=skip_count
+    )
 
-   # Extract the DP value from the Info column
-    mpileup_data['DP'] = mpileup_data['INFO'].str.extract(r'DP=(\d+)')
+    # Extract the DP value from the Info column
+    mpileup_data["DP"] = mpileup_data["INFO"].str.extract(r"DP=(\d+)")
 
     # Convert the DP column to numeric,
     # setting errors to 'coerce' which replaces non-convertible values with NaN
-    mpileup_data['DP'] = pd.to_numeric(mpileup_data['DP'], errors='coerce')
+    mpileup_data["DP"] = pd.to_numeric(mpileup_data["DP"], errors="coerce")
 
     # Replace NaN values with -1 (or another placeholder if -1 is not suitable)
-    mpileup_data['DP'].fillna(-1, inplace=True)
+    mpileup_data["DP"].fillna(-1, inplace=True)
 
     # Convert to integer
-    mpileup_data['DP'] = mpileup_data['DP'].astype(int)
+    mpileup_data["DP"] = mpileup_data["DP"].astype(int)
 
     # Select only the Chromosome, Position, and DP columns
-    extracted_data = mpileup_data[['CHROM', 'POS', 'DP']]
+    extracted_data = mpileup_data[["CHROM", "POS", "DP"]]
 
     # Save the extracted data to a tab-delimited file
-    extracted_data.to_csv(output_file, sep='\t', index=False)
+    extracted_data.to_csv(output_file, sep="\t", index=False)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -40,4 +42,3 @@ if __name__ == "__main__":
     input_file = sys.argv[1]
     output_file = sys.argv[2]
     extract_mpileup_info(input_file, output_file)
-
